fcbf68bf6b3c398625c03edcfdf96c697e742725
groovebass <groovebassist1@gmail.com>
2023-10-10 15:03:10 +0200

    [Scaffolding]: Starter template

---
+++ data/categories-data.ts
@@ -0,0 +1,55 @@
+const categories = [
+  {
+    desc: "Fresh greens, crisp veggies, and delectable dressings for a light and healthy start.",
+    id: "cln364xd200006ulsx6i55j5i",
+    category: "Burgers",
+    imageSrc: "/img/categories/burger.png",
+  },
+  {
+    desc: "Juicy, flavorful patties served in a bun with various toppings and condiments.",
+    id: "cln3677fn00016uls4vp6dhx1",
+    imageSrc: "/img/categories/pizza.png",
+  },
+  {
+    desc: "Italian classics featuring various pasta shapes paired with rich sauces and ingredients.",
+    id: "cln367ea600026uls7jfp2oh3",
+    category: "Drinks",
+    imageSrc: "/img/categories/drink.png",
+  },
+  {
+    desc: "Fresh catches of the day, prepared in diverse styles, from grilled to fried.",
+    id: "cln367k2g00036ulsnpzje4lb",
+    category: "Sushi",
+    imageSrc: "/img/categories/sushi.png",
+  },
+  {
+    desc: "Premium cuts of meat, cooked to perfection, served with complementary sauces.",
+    id: "cln367py300046uls02met36p",
+    category: "Fast Food",
+    imageSrc: "/img/categories/fast-food.png",
+  },
+  {
+    desc: "Thin or thick crusts topped with an array of savory ingredients and melted cheese.",
+    id: "cln367vh100056ulsp11ktr38",
+    category: "Chicken",
+    imageSrc: "/img/categories/chicken.png",
+  },
+  {
+    desc: "Plant-based dishes showcasing the flavors and textures of vegetables and legumes.",
+    id: "cln36833f00066ulsvje412h6",
+    category: "Pasta",
+    imageSrc: "/img/categories/pasta.png",
+  },
+  {
+    desc: "Artful, bite-sized delights featuring fresh fish, rice, and other exquisite ingredients.",
+    id: "cln3688yr00076ulsgiydrs53",
+    category: "Steak",
+    imageSrc: "/img/categories/steak.png",
+  },
+  {
+    desc: "Spicy and vibrant dishes like tacos, burritos, and enchiladas with bold flavors.",
+    id: "cln368eyq00086ulsvtqpi1ur",
+    category: "Vegan",
+    imageSrc: "/img/categories/vegan.png",
+  },
+];
---
+++ data/menu-data.ts
@@ -0,0 +1,98 @@
+export const MenuData = [
+    {
+      id: "6bf56de4-7ed2-4ece-b45b-5001b52a99bab",
+      title: "Chaw Burger",
+      shortDescr: "smoked cherries dumpling  with a delicate texture",
+      longDescr:
+        "marinated oats quesadilla is a little greasy with a tough texture. It has subtle hints of wheat with myrtle plant and has a dash of calamint",
+      price: 104,
+      image: "/img/food/burger.png",
+      category: "breakfast",
+  
+      prepType: ["well-done", "medium"],
+    },
+    {
+      id: "e08155e0-8708-47c3-8234-09b30f147f64",
+      title: "Chicken Fillet",
+      shortDescr: "wheat with myrtle plant and has a dash of calamint.",
+      longDescr:
+        "marinated oats quesadilla is a little greasy with a tough texture. It has subtle hints of wheat with myrtle plant and has a dash of calamint",
+      price: 71,
+      image: "/img/food/chicken-fillet.png",
+      category: "meals",
+  
+      prepType: ["well-done", "medium"],
+    },
+    {
+      id: "df239466-5617-4f59-9adf-2478202ef94f",
+      title: "Rockshandy",
+      shortDescr: "chickpeas with sumac and has lemon balm",
+      longDescr:
+        "marinated oats quesadilla is a little greasy with a tough texture. It has subtle hints of wheat with myrtle plant and has a dash of calamint",
+      price: 41,
+      image: "/img/food/rockshandy.png",
+      category: "beverages",
+  
+      prepType: ["well-done", "medium"],
+    },
+    {
+      id: "ed3b3942-5ba2-4b33-b908-b768abbe086b",
+      title: "Big Steak",
+      shortDescr: "chickpeas with sumac and has lemon balm",
+      longDescr:
+        "marinated oats quesadilla is a little greasy with a tough texture. It has subtle hints of wheat with myrtle plant and has a dash of calamint",
+      price: 39,
+      image: "/img/food/steak.png",
+      category: "meat",
+  
+      prepType: ["well-done", "medium"],
+    },
+    {
+      id: "95ede0b1-f57f-4e6e-9181-be8ee5f40be3",
+      title: "Louis Steak",
+      shortDescr: "smoked cherries dumpling  with a delicate texture",
+      longDescr:
+        "marinated oats quesadilla is a little greasy with a tough texture. It has subtle hints of wheat with myrtle plant and has a dash of calamint",
+      price: 110,
+      image: "/img/food/steak2.png",
+      category: "bbq",
+  
+      prepType: ["well-done", "medium"],
+    },
+    {
+      id: "8e386904-6b1f-47e8-ba3a-56f0cb115d1c",
+      title: "SandWich",
+      shortDescr: "chickpeas with sumac and has lemon balm",
+      longDescr:
+        "marinated oats quesadilla is a little greasy with a tough texture. It has subtle hints of wheat with myrtle plant and has a dash of calamint",
+      price: 35,
+      image: "/img/food/sandwich.png",
+      category: "breakfast",
+  
+      prepType: ["well-done", "medium"],
+    },
+    {
+      id: "f674e9c9-6ae1-43fd-a1ce-1828cd64ae59",
+      title: "Salmond ",
+      shortDescr: "chickpeas with sumac and has lemon balm",
+      longDescr:
+        "marinated oats quesadilla is a little greasy with a tough texture. It has subtle hints of wheat with myrtle plant and has a dash of calamint",
+      price: 113,
+      image: "/img/food/salmond.png",
+      category: "meals",
+  
+      prepType: ["well-done", "medium"],
+    },
+    {
+      id: "989040c0-95bb-4a1b-a5ad-42653124b921",
+      title: "Mojito",
+      shortDescr: "wheat with myrtle plant and has a dash of calamint.",
+      longDescr:
+        "marinated oats quesadilla is a little greasy with a tough texture. It has subtle hints of wheat with myrtle plant and has a dash of calamint",
+      price: 56,
+      image: "/img/food/mojito.png",
+      category: "drinks",
+  
+      prepType: ["well-done", "medium"],
+    },
+  ];
